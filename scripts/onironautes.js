e9aeb99c469a268b4cc8961e12a55dc98058c3fe
Sunny Ripert <sunny@sunfox.org>
2024-03-25 09:18:58 +0100

    Hide content when playing a video

--- scripts/onironautes.js
+++ scripts/onironautes.js
@@ -19,4 +19,29 @@ function handleEvents() {
   })
 }
 
+// This can be replaced by the `:playing` CSS pseudo-class when there is more
+// browser support.
+// https://developer.mozilla.org/en-US/docs/Web/CSS/:playing
+function handleVideoPlay() {
+  const playing = () => { document.body.classList.add("is-video-playing") }
+  const stopped = () => { document.body.classList.remove("is-video-playing") }
+
+  document.querySelectorAll("video").forEach(video => {
+    video.addEventListener("play", playing)
+    video.addEventListener("pause", stopped)
+    video.addEventListener("ended", stopped)
+  })
+}
+
+// Pause all videos when clicking outside of them.
+function pauseOnClickOutsideVideo() {
+  document.addEventListener("click", event => {
+    if (!event.target.closest("video")) {
+      document.querySelectorAll("video").forEach(video => video.pause())
+    }
+  })
+}
+
 handleEvents()
+handleVideoPlay()
+pauseOnClickOutsideVideo()
